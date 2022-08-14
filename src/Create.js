b1607abdaf01590f6ae034833cb2b349ebd88226
Teguh Setiawan <teguhsetiawan@Teguhs-Air.lan>
2022-08-14 21:43:37 +0700

    form on create

--- src/Create.js
+++ src/Create.js
@@ -1,6 +1,32 @@
+import { useState } from "react";
+
 const Create = () => {
-    return ( <div>
+    const [title, setTitle] = useState('');
+    const [body, setBody] = useState('');
+    const [author, setAuthor] = useState('mario');
+
+
+    return ( <div className="create">
         <h2>Create new blogs</h2>
+        <form action="#">
+            <label htmlFor="title">Title</label>
+            <input type="text" name="title" id="title" value={title} onChange={(e) => {setTitle(e.target.value)}} />
+            
+            <label htmlFor="body">Body</label>
+            <textarea name="body" id="body" value={body} onChange={(e) => setBody(e.target.value)}></textarea>
+            
+            <label htmlFor="author">author</label>
+            <select name="author" id="author" value={author} onChange={(e) => setAuthor(e.target.value)}>
+                <option value="mario">mario</option>
+                <option value="yosho">yosho</option>
+                <option value="luigi">luigi</option>
+            </select>
+
+        </form>
+
+        <p>{title}</p>
+        <p>{body}</p>
+        <p>{author}</p>
     </div> );
 }
  
