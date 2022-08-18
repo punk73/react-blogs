5089cc74ccd0199ba07f9b391e7e5a1a7ea81da8
Teguh Setiawan <teguhsetiawan@Teguhs-Air.lan>
2022-08-18 20:00:26 +0700

    successfully add blog

--- src/Create.js
+++ src/Create.js
@@ -1,14 +1,38 @@
 import { useState } from "react";
+import { useHistory } from "react-router-dom";
 
 const Create = () => {
     const [title, setTitle] = useState('');
     const [body, setBody] = useState('');
     const [author, setAuthor] = useState('mario');
+    const [loading, setLoading] = useState(false);
 
+    const history = useHistory();
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        const blog = {title, body, author};
+        setLoading(true);
+        fetch("http://localhost:8000/blogs", {
+            method : "POST",
+            headers : {'Content-Type':"application/json"},
+            body : JSON.stringify(blog)
+        }).then(res => res.json())
+            .then(data=>{
+                console.log(data)
+            })
+            .catch(err => {
+                console.log(err)
+            })
+            .finally(() => {
+                setLoading(false); 
+                history.push('/')
+            })
+    }
 
     return ( <div className="create">
         <h2>Create new blogs</h2>
-        <form action="#">
+        <form onSubmit={handleSubmit}>
             <label htmlFor="title">Title</label>
             <input type="text" name="title" id="title" value={title} onChange={(e) => {setTitle(e.target.value)}} />
             
@@ -22,6 +46,8 @@ const Create = () => {
                 <option value="luigi">luigi</option>
             </select>
 
+            {!loading && <button type="submit">Submit</button>}
+            {loading && <span>loading...</span> }
         </form>
 
         <p>{title}</p>
